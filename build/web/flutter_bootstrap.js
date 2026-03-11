b04686e5208730133448588650655571fcf15562
Bright Dxmien Obesi <106479423+brightdxmien@users.noreply.github.com>
2026-03-11 12:48:32 +0100

    s

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2130326912" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3087146744" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
